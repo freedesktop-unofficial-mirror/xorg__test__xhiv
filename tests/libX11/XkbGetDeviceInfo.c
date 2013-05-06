58e6058462aa0de727ac887d414c18bdab02ac74
Alan Coopersmith <alan.coopersmith@oracle.com>
2013-05-05 17:07:17 -0700

    Move Xkb extension setup to tests/libX11/xhiv-Xkb.h header
    Signed-off-by: Alan Coopersmith <alan.coopersmith@oracle.com>

--- tests/libX11/XkbGetDeviceInfo.c
+++ tests/libX11/XkbGetDeviceInfo.c
@@ -25,6 +25,7 @@
 #endif
 
 #include "xhiv.h"
+#include "xhiv-Xkb.h"
 #include <X11/Xlib.h>
 #include <X11/XKBlib.h>
 #include <X11/Xproto.h>
@@ -32,45 +33,9 @@
 #include <assert.h>
 #include <stdio.h>
 
-#define MY_XKB_EXT_CODE         200
-
 static void
 testOverflowFields(void)
 {
-    /* Report that XKB is present */
-    const xQueryExtensionReply qext_reply = {
-        .type = X_Reply,
-        .length = 0,
-        .present = xTrue,
-        .major_opcode = MY_XKB_EXT_CODE,
-        .first_event = 100,
-        .first_error = 150
-    };
-    xhiv_response qext_response = {
-        .reqType = X_QueryExtension,
-        .reqMinor = XHIV_REQ_IGNORE,
-        .sequence = XHIV_SEQ_MATCHDATA,
-        .match_data = XkbName,
-        .length = (SIZEOF(xQueryExtensionReply) >> 2),
-        .response_data = &qext_reply,
-        .response_datalen = sizeof(qext_reply)
-    };
-    /* Report which version of XKB we support */
-    const xkbUseExtensionReply xkb_use_reply = {
-        .type = X_Reply,
-        .supported = xTrue,
-        .serverMajor = 1,
-        .serverMinor = 0
-    };
-    xhiv_response xkb_use_response = {
-        .next = &qext_response,
-        .reqType = MY_XKB_EXT_CODE,
-        .reqMinor = X_kbUseExtension,
-        .sequence = XHIV_SEQ_IGNORE,
-        .length = (SIZEOF(xkbUseExtensionReply) >> 2),
-        .response_data = &xkb_use_reply,
-        .response_datalen = sizeof(xkb_use_reply)
-    };
     /* xkbGetDeviceInfo requires a length-counted name string follows */
     struct {
         uint16_t length;
